6e30366d2a3a30e12f1d1e36b818cadf8ac53544
ovdanalo <104578306+ovdanalo@users.noreply.github.com>
2022-12-10 14:47:57 +0100

    add scrollbarY-linked navbar

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const subscribe = document.getElementById("popup-subscribe-container").classList
 const subscribePr = document.getElementById("popup-subscribe-primary").classList;
 const search = document.getElementById("popup-search").classList;
 
+const navbar_primary = document.getElementById("navbar-primary").classList;
+const sticky_addon = document.getElementById("sticky-addon").classList;
+const center_nav_secondary = document.getElementById("center-nav-secondary").classList;
+const subscribe_link = document.getElementById("subscribe").classList;
+const logo_primary = document.getElementById("logo-primary").classList;
+
 function showMenu() {
     menuClosed.toggle("show");
     menuOpen.toggle("hide");
@@ -59,3 +65,21 @@ function overlayTrClick() {
     subscribe.remove("show");
     search.remove("show");
 }
+
+window.addEventListener("scroll", function (event) {
+    let top = window.scrollY;
+    console.log(top);
+    if (top == 300 || top == 350 || top == 400) {
+        navbar_primary.add("sticky", "sticky-navbar");
+        sticky_addon.add("show");
+        center_nav_secondary.add("hide");
+        subscribe_link.add("hide");
+        logo_primary.add("show", "sticky-mar-left");
+    } else if (top == 200 || top == 100 || top == 0) {
+        navbar_primary.remove("sticky", "sticky-navbar");
+        sticky_addon.remove("show");
+        center_nav_secondary.remove("hide");
+        subscribe_link.remove("hide");
+        logo_primary.remove("show", "sticky-mar-left");
+    }
+}, false);
