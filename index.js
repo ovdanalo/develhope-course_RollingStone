13e4f70a27e327c6b553d6abb7478b716d9d9059
ovdanalo <104578306+ovdanalo@users.noreply.github.com>
2022-12-13 11:20:58 +0100

    implement carousel in section 3

--- index.js
+++ index.js
@@ -82,3 +82,20 @@ window.addEventListener("scroll", function (event) {
         logo_primary.remove("show", "sticky-mar-left");
     }
 }, false);
+
+
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
